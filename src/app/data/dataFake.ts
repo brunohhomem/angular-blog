9e2a1d42071b13513475d875c6b9633e14db10ff
Bruno H. Homem <brunohhomem@gmail.com>
2023-10-15 10:23:10 -0300

    new footer, content and dataFake

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,25 @@
+export const dataFake = [
+  {
+    "id":1,
+    "photo": "https://miro.medium.com/v2/resize:fit:1400/0*MGGK5bqMTtobxsAS",
+    "title": "ANGULAR'S TEAM.",
+    "description":"HAUSHDAUSHDUHAUDHUAHSDUHAUSDHUAHRUAUHSDJKAHSDJKHASHUASDANSDMABSDHAUSDHA. HAUSHDAUSHDUHAUDHUAHSDUHAUSDHUAHRUAUHSDJKAHSDJKHASHUASDANSDMABSDHAUSDHA.HAUSHDAUSHDUHAUDHUAHSDUHAUSDHUAHRUAUHSDJKAHSDJKHASHUASDANSDMABSDHAUSDHA"
+  }
+  // ,
+  // {
+  //   "id":2,
+  //   "photo": "",
+  //   "title": "",
+  //   "description":""
+  // },  {
+  //   "id":3,
+  //   "photo": "",
+  //   "title": "",
+  //   "description":""
+  // },  {
+  //   "id":4,
+  //   "photo": "",
+  //   "title": "",
+  //   "description":""
+  // },
+]
